257f67edcb2aa6ca9dc2937a23eb161693155bac
igor <igor.kuleba@gmail.com>
2020-12-14 09:51:57 +0100

    Added new unit test project CurrencyExchangeOffice.Tests and class SymbolTests in it.

---
+++ CurrencyExchangeOffice.Tests/SymbolTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CurrencyExchangeOffice.Tests
+{
+    public class SymbolTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
