036719c18e942191ecee410fa48981c2b4970236
igor <igor.kuleba@gmail.com>
2020-12-14 12:04:27 +0100

    Added in class Currency:   - constructor: private Currency(Symbol symbol, CurrencyName currencyName),   - factory method: public static Currency Of(Symbol currencySymbol, CurrencyName currencyName),   - generated equality members.

--- CurrencyExchangeDomain/Currency.cs
+++ CurrencyExchangeDomain/Currency.cs
@@ -1,9 +1,45 @@
+using System;
+
 namespace CurrencyExchangeDomain
 {
     public sealed class Currency
     {
         public Symbol Symbol { get; }
         public CurrencyName CurrencyName { get; }
-        
+
+        private Currency(Symbol symbol, CurrencyName currencyName)
+        {
+            Symbol = symbol;
+            CurrencyName = currencyName;
+        }
+
+        public static Currency Of(Symbol currencySymbol, CurrencyName currencyName)
+        {
+            if (currencySymbol == null)
+            {
+                throw new ArgumentNullException($"{nameof(currencySymbol)} cannot be null.");
+            }
+
+            if (currencyName == null)
+            {
+                throw new ArgumentNullException($"{nameof(currencyName)} cannot be null.");
+            }
+            return new Currency(currencySymbol, currencyName);
+        }
+
+        private bool Equals(Currency other)
+        {
+            return Equals(Symbol, other.Symbol) && Equals(CurrencyName, other.CurrencyName);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is Currency other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Symbol, CurrencyName);
+        }
     }
 }
