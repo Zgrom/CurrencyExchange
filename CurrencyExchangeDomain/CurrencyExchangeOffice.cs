1ccfb8973315f6fe68007804a755e9a9c247ce74
igor <igor.kuleba@gmail.com>
2020-12-14 09:13:18 +0100

    Added attributes of CurrencyExchangeOffice class:   - public List<Currency> AllAvailableCurrencies   - public Currency BaseCurrency   - public Currency TargetCurrency   - private Timestamp Timestamp   - public Rate Rate   - public Amount BaseCurrencyAmount   - public Amount TargetCurrencyAmount

--- CurrencyExchangeDomain/CurrencyExchangeOffice.cs
+++ CurrencyExchangeDomain/CurrencyExchangeOffice.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CurrencyExchangeDomain
 {
-    public class CurrencyExchangeOffice
+    public sealed class CurrencyExchangeOffice
     {
+        public List<Currency> AllAvailableCurrencies { get; }
+        public Currency BaseCurrency { get; }
+        public Currency TargetCurrency { get; }
+        private Timestamp Timestamp { get; }
+        public Rate Rate { get; }
+        public Amount BaseCurrencyAmount { get; }
+        public Amount TargetCurrencyAmount { get; }
     }
 }
