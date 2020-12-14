7a09d0fca8d2166054e84b1289ecdb3d533d3fcf
igor <igor.kuleba@gmail.com>
2020-12-14 11:00:29 +0100

    Created class CurrencyName with:   - attribute: public string CurrencyNameValue,   - constructor: private CurrencyName(string currencyNameValue),   - factory method: public static CurrencyName From(string currencyName),   - generated equality members.

---
+++ CurrencyExchangeDomain/CurrencyName.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace CurrencyExchangeDomain
+{
+    public sealed class CurrencyName
+    {
+        public string CurrencyNameValue { get; }
+
+        private CurrencyName(string currencyNameValue)
+        {
+            CurrencyNameValue = currencyNameValue;
+        }
+
+        public static CurrencyName From(string currencyName)
+        {
+            if (string.IsNullOrWhiteSpace(currencyName))
+            {
+                throw new ArgumentException(
+                    $"{nameof(currencyName)} cannot be null or white space.");
+            }
+            
+            return new CurrencyName(currencyName);
+        }
+
+        private bool Equals(CurrencyName other)
+        {
+            return CurrencyNameValue == other.CurrencyNameValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is CurrencyName other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return (CurrencyNameValue != null ? CurrencyNameValue.GetHashCode() : 0);
+        }
+    }
+}
