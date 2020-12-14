3debf649373069548bbd38b956439d270072b9e7
igor <igor.kuleba@gmail.com>
2020-12-14 09:22:22 +0100

    Added Currency class and its attributes:   - public Symbol Symbol,   - public CurrencyName CurrencyName.

---
+++ CurrencyExchangeDomain/Currency.cs
@@ -0,0 +1,9 @@
+namespace CurrencyExchangeDomain
+{
+    public sealed class Currency
+    {
+        public Symbol Symbol { get; }
+        public CurrencyName CurrencyName { get; }
+        
+    }
+}
