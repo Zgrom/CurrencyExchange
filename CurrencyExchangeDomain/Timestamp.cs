66bba9d7a981cc2508bb3d312453bee3b71606c8
igor <igor.kuleba@gmail.com>
2020-12-15 08:40:07 +0100

    Created class Timestamp with:   - attribute: public long TimestampValue,   - constructor: private Timestamp(long timestampValue),   - factory method: public static Timestamp From(long timestamp),   - static method: private static Timestamp Now(),   - method: public bool IsTooOld()   - generated equality members.

---
+++ CurrencyExchangeDomain/Timestamp.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace CurrencyExchangeDomain
+{
+    public class Timestamp
+    {
+        public long TimestampValue { get; }
+
+        private Timestamp(long timestampValue)
+        {
+            TimestampValue = timestampValue;
+        }
+
+        public static Timestamp From(long timestamp)
+        {
+            if (timestamp < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    $"{nameof(timestamp)} cannot have negative value.");
+            }
+            return new Timestamp(timestamp);
+        }
+
+        private static Timestamp Now()
+        {
+            var timestampNow = DateTimeOffset.Now.ToUnixTimeSeconds();
+            return new Timestamp(timestampNow);
+        }
+
+        public bool IsTooOld()
+        {
+            const int period = 86400; // exactly one day in seconds
+            return Now().TimestampValue - TimestampValue > period;
+        }
+
+        protected bool Equals(Timestamp other)
+        {
+            return TimestampValue == other.TimestampValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Timestamp) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return TimestampValue.GetHashCode();
+        }
+    }
+}
