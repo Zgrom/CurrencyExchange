6b0521e38e33bd264e9ffd18f99b949582428a07
igor <igor.kuleba@gmail.com>
2020-12-16 08:05:48 +0100

    Renamed unit test project from CurrencyExchangeOffice.Tests to CurrencyExchangeDomain.Tests.

--- CurrencyExchangeDomain/CurrencyExchangeOffice.cs
+++ CurrencyExchangeDomain/CurrencyExchangeOffice.cs
@@ -12,5 +12,62 @@ namespace CurrencyExchangeDomain
         public Rate Rate { get; }
         public Amount BaseCurrencyAmount { get; }
         public Amount TargetCurrencyAmount { get; }
+
+        private CurrencyExchangeOffice(
+            List<Currency> allAvailableCurrencies,
+            Currency baseCurrency,
+            Currency targetCurrency,
+            Timestamp timestamp,
+            Rate rate,
+            Amount baseCurrencyAmount,
+            Amount targetCurrencyAmount)
+        {
+            AllAvailableCurrencies = allAvailableCurrencies;
+            BaseCurrency = baseCurrency;
+            TargetCurrency = targetCurrency;
+            Timestamp = timestamp;
+            Rate = rate;
+            BaseCurrencyAmount = baseCurrencyAmount;
+            TargetCurrencyAmount = targetCurrencyAmount;
+        }
+
+        public static CurrencyExchangeOffice Of(
+            List<Currency> allAvailableCurrencies,
+            Currency baseCurrency,
+            Currency targetCurrency,
+            Timestamp timestamp,
+            Rate rate,
+            Amount baseCurrencyAmount)
+        {
+            if (baseCurrency == null)
+            {
+                throw new ArgumentException($"{nameof(baseCurrency)} cannot be null.");
+            }
+            if (targetCurrency == null)
+            {
+                throw new ArgumentException($"{nameof(targetCurrency)} cannot be null.");
+            }
+            if (timestamp == null)
+            {
+                throw new ArgumentException($"{nameof(timestamp)} cannot be null.");
+            }
+            if (rate == null)
+            {
+                throw new ArgumentException($"{nameof(rate)} cannot be null.");
+            }
+            if (baseCurrencyAmount == null)
+            {
+                throw new ArgumentException($"{nameof(baseCurrencyAmount)} cannot be null.");
+            }
+            var targetCurrencyAmount = baseCurrencyAmount.MultiplyWithRate(rate);
+            return new CurrencyExchangeOffice(
+                allAvailableCurrencies,
+                baseCurrency,
+                targetCurrency,
+                timestamp,
+                rate,
+                baseCurrencyAmount,
+                targetCurrencyAmount);
+        }
     }
 }
