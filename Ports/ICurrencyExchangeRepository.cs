fe23bc058e8f23b09d1032efdcc8756b10144335
igor <igor.kuleba@gmail.com>
2020-12-17 08:24:31 +0100

    Added Ports project and ICurrencyExchangeRepository interface in it.

---
+++ Ports/ICurrencyExchangeRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CurrencyExchangeDomain;
+
+namespace Ports
+{
+    public interface ICurrencyExchangeRepository
+    {
+        Task<CurrencyExchange> GetCurrencyExchangeRate(
+            Currency baseCurrency, 
+            Currency targetCurrency,
+            Amount baseCurrencyAmount);
+        Task InsertCurrencyExchange(CurrencyExchange currencyExchange);
+        Task DeleteCurrencyExchange(CurrencyExchange currencyExchange);
+        Task<List<Currency>> GetAllAvailableCurrencies();
+    }
+}
