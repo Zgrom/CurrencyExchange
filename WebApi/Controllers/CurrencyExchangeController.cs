2779031461e4cf8fe5384fadf65380cda136f202
igor <igor.kuleba@gmail.com>
2020-12-21 18:02:58 +0100

    Created Web API project and added in it:   - CurrencyExchangeController : ControllerBase class,   - CurrencyDto class.

---
+++ WebApi/Controllers/CurrencyExchangeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyExchangeController : ControllerBase
+    {
+        public CurrencyExchangeController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using var client = new HttpClient();
+            var content = await client.GetStringAsync(
+                "http://data.fixer.io/api/symbols?access_key=ecd46d5e28c44d88658a7b109cc29b2a");
+            return Ok(content);
+        }
+    }
+}
---
+++ WebApi/Dto/CurrencyDto.cs
@@ -0,0 +1,8 @@
+namespace WebApi.Dto
+{
+    public sealed class CurrencyDto
+    {
+        public string CurrencySymbol { get; set; }
+        public string CurrencyName { get; set; }
+    }
+}
